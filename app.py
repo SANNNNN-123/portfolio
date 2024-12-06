056d44fb029832224696e1ab00ed22c1dd92a98b
SANNNNN-123 <zuhairaziz95@yahoo.com>
2024-12-06 16:20:45 +0800

    edited version

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    current_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    return render_template('index.html', current_time=current_time)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
