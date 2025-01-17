b4aa68465d36f8cc046527bf58656847352d78bc
SANNNNN-123 <zuhairaziz95@yahoo.com>
2025-01-18 01:42:42 +0800

    fix deployment

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 from datetime import datetime
 
 app = Flask(__name__)
@@ -76,5 +76,5 @@ def index():
     return render_template('index.html', current_time=current_time)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
 
